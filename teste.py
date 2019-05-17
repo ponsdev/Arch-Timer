b2e3b1efa75baecb111cfab24344e592c5aca0d2
Vinicius Pons <viniciusdev@zoho.com>
2019-05-17 16:17:36 -0300

    teste1

--- teste.py
+++ teste.py
@@ -1,4 +1,31 @@
-import sys
+# postgresql
+# import psycopg2
 
-filePath=sys.argv[1]
-print(filePath)
+# con = psycopg2.connect(
+#     host="localhost",
+#     database="nomeDB",
+#     user="user",
+#     password="password")
+
+# cur = con.cursor()
+
+# cur.execute("select id, name from employees")
+
+# rows = cur.fetchall()
+# for r in rows:
+#     print(f"id {r[0]} name {r[1]}")
+
+# con.close()
+
+# mongoDB
+import pymongo
+
+connection = pymongo.MongoClient('localhost', 27017)
+database = connection['archtimer']
+collection = database['clients']
+
+data = {'Name': 'SM_Cristal'}
+
+collection.insert_one(data)
+
+connection.close()
